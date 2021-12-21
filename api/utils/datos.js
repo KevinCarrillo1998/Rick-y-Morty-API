e1d61900dd9d2db2a3c7b35697698af76671cc52
Kevin Carrillo Davila <kevincarrillodavila@gmail.com>
2021-12-20 20:02:20 -0800

    Ya estoy terminando el proyecto

--- api/utils/datos.js
+++ api/utils/datos.js
@@ -4,11 +4,12 @@ const cardGenerator = object => {
   card.innerHTML = `
     <div class="card-image">
       <img class="img-fluid" src="${object.image}" alt="imagen de ${object.name}">
-      <span class="badge-top">${object.origin.name}</span>
     </div>
     <div class="card-body">
       <h3>${object.name}</h3>
       <p>${object.species}</p>
+      <p>${object.origin.name}</p>
+      <br>
       <span class="${object.status.toLowerCase()}">${object.status}</span>
     </div>
   `;
